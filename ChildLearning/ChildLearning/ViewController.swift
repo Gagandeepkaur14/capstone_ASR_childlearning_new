8c7150571658e79949a5f8128c84caf5c147a9ff
Gagandeepkaur14 <77646825+Gagandeepkaur14@users.noreply.github.com>
2021-04-11 14:01:14 +0530

    main view functionality

--- ChildLearning/ChildLearning/ViewController.swift
+++ ChildLearning/ChildLearning/ViewController.swift
@@ -8,11 +8,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnSignUp: UIButton!
+    @IBOutlet weak var btnLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
+        btnLogin.layer.cornerRadius = btnLogin.frame.size.height/2
+        btnSignUp.layer.borderWidth = 1
+        btnSignUp.layer.cornerRadius = btnSignUp.frame.size.height/2
+        btnSignUp.layer.borderColor = UIColor.white.cgColor
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func loginClicked(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(identifier: "LoginViewController") as! LoginViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
 
