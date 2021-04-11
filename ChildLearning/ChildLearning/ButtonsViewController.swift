fa4927876911d965dba1478cff57c2c6fb2a0257
Gagandeepkaur14 <77646825+Gagandeepkaur14@users.noreply.github.com>
2021-04-11 13:57:23 +0530

    multi button view design

---
+++ ChildLearning/ChildLearning/ButtonsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ButtonsViewController.swift
+//  ChildLearning
+//
+//  Created by Rakinder on 11/04/21.
+//
+
+import UIKit
+
+class ButtonsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
