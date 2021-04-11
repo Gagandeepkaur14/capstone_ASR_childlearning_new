3654ba37a48c57659db1c5fa1ef1c7a065f0e859
Amandeep98781 <77733148+Amandeep98781@users.noreply.github.com>
2021-04-11 22:25:30 +0530

    fetch user in signup

--- ChildLearning/ChildLearning/SignUpViewController.swift
+++ ChildLearning/ChildLearning/SignUpViewController.swift
@@ -5,6 +5,7 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class SignUpViewController: UIViewController {
 
@@ -12,6 +13,8 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var txtFName: UITextField!
     @IBOutlet weak var txtFEmail: UITextField!
     @IBOutlet weak var txtFPassword: UITextField!
+    var ref: DatabaseReference!
+    var users: [Users] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +22,22 @@ class SignUpViewController: UIViewController {
         self.navigationController?.navigationBar.isTranslucent = true
         // Do any additional setup after loading the view.
     }
+    
+    func fetchAllUsers(){
+        /*
+         Get the all data from Firebase
+         */
+        ref.child(Constant.FirebaseData.User).observe(.value, with: { snapshot in
+       
+            for eachPlace in (snapshot.children){
+                let place = Users(snapshot: eachPlace as! DataSnapshot)
+                self.users.append(place)
+                
+            }
+           
+        })
+    }
+    
     
     @IBAction func backClicked(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
