9970dc998530b494ffbf7f279d79c8dcea9b183b
Gagandeepkaur14 <77646825+Gagandeepkaur14@users.noreply.github.com>
2021-04-11 14:03:23 +0530

    add the corner radius

--- ChildLearning/ChildLearning/HomeViewController.swift
+++ ChildLearning/ChildLearning/HomeViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var lblUserName: UILabel!
+    @IBOutlet var btnsHome: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        for i in btnsHome{
+            i.layer.cornerRadius = i.frame.size.height/2
+            i.layer.borderWidth = 1
+            i.layer.cornerRadius = i.frame.size.height/2
+            i.layer.borderColor = UIColor.white.cgColor
+        }
         // Do any additional setup after loading the view.
     }
     
