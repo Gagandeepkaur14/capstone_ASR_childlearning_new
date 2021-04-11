354b8390337d9165c519ebabdd570817b21e320d
Amandeep98781 <77733148+Amandeep98781@users.noreply.github.com>
2021-04-11 22:01:26 +0530

    Firebase Model

---
+++ ChildLearning/ChildLearning/FirebaseModel.swift
@@ -0,0 +1,28 @@
+//
+//  FirebaseModel.swift
+//  ChildLearning
+//
+//
+
+import UIKit
+import FirebaseDatabase
+
+struct Users {
+    var name = ""
+    var email = ""
+    var id = ""
+    var password = ""
+    init(name: String, email: String, password: String) {
+        self.name = name
+        self.email = email
+       // self.id = id
+         self.password = password
+    }
+    init(snapshot: DataSnapshot) {
+        let snapshotValue = snapshot.value as! [String:AnyObject]
+        name = snapshotValue["name"] as? String ?? ""
+        email = snapshotValue["email"] as? String ?? ""
+        password = snapshotValue["password"] as? String ?? ""
+
+    }
+}
