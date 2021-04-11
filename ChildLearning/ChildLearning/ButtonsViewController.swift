7713f03d76265db879e5ab07aff55c20b1241726
Gagandeepkaur14 <77646825+Gagandeepkaur14@users.noreply.github.com>
2021-04-11 14:08:55 +0530

    collection view delegate and datasource

--- ChildLearning/ChildLearning/ButtonsViewController.swift
+++ ChildLearning/ChildLearning/ButtonsViewController.swift
@@ -19,3 +19,27 @@ class ButtonsViewController: UIViewController {
     
 
 }
+
+extension ButtonsViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ButtonCollectionViewCell", for: indexPath) as! ButtonCollectionViewCell
+        cell.layer.cornerRadius = 5.0
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = self.storyboard?.instantiateViewController(identifier: "SpeakViewController") as! SpeakViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension ButtonsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: self.view.frame.width/2 - 20, height: self.view.frame.width/2 - 20)
+       
+    }
+}
