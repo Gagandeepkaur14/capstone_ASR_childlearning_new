abb053baa3c98c379fed3acf7ec89b4af2d4bf62
Gagandeepkaur14 <77646825+Gagandeepkaur14@users.noreply.github.com>
2021-04-11 13:55:18 +0530

    home view design

---
+++ ChildLearning/ChildLearning/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  ChildLearning
+//
+//  Created by Rakinder on 11/04/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func wordClicked(_ sender: Any) {
+    }
+    
+    @IBAction func alphabetClicked(_ sender: Any) {
+    }
+    
+    @IBAction func numberClicked(_ sender: Any) {
+    }
+    
+    @IBAction func logoutClicked(_ sender: Any) {
+
+    }
+
+}
